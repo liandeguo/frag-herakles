d14cc4fec325638b503a4f21c211039ceb548f4b
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-03-01 08:28:27 +0100

    Added connection to PSQL Database

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "192.168.1.174"
+	port     = 5432
+	user     = "postgres"
+	password = "9PstG1s>N(50zo3v"
+	dbname   = "frag-herakles"
 )
 
 func handleAnswer(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +21,22 @@ func handleAnswer(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 	http.HandleFunc("/answer", handleAnswer)
-	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
